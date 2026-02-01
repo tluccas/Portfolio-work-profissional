4aab0029aaeef170ae1f9b5070c58d55bf1df6c6
Lucas Alves <lucas1901alves@gmail.com>
2026-01-31 23:58:45 -0300

    feat(section-id): add section id

--- portfolio-app/components/sections/reviews/Reviews.tsx
+++ portfolio-app/components/sections/reviews/Reviews.tsx
@@ -9,7 +9,7 @@ const ReviewsGrid = dynamic(() => import("./ReviewsGrid"), {
 
 export default function Reviews() {
   return (
-    <section className="relative flex flex-col isolate items-center justify-start overflow-hidden bg-background pt-8 pb-40 lg:pt-2">
+    <section id="avaliacoes" className="relative flex flex-col isolate items-center justify-start overflow-hidden bg-background pt-8 pb-40 lg:pt-2">
       <div className="relative z-10 container mx-auto px-6 flex flex-col items-center">
         <SectionTitle text="Avaliações de Pacientes" highlight="Pacientes" />
         <ReviewsGrid />
