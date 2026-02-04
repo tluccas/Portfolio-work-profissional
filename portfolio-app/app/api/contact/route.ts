9d9b7b33bded06af3de9b4db27551fcf4eaea80d
Lucas Alves <lucas1901alves@gmail.com>
2026-02-04 01:05:07 -0300

    feat(api): add req validation with zod

--- portfolio-app/app/api/contact/route.ts
+++ portfolio-app/app/api/contact/route.ts
@@ -1,9 +1,25 @@
 import nodemailer from "nodemailer";
+import { z } from "zod";
 
+const schema = z.object({
+  name: z.string().min(3, "Name is required"),
+  email: z.email("Invalid email address"),
+  phone: z.string().optional(),
+  message: z.string().optional(),
+});
 export async function POST(req: Request) {
   const { name, email, phone, message } = await req.json();
 
-  if (!name || !email) {
+  const parseResult = schema.safeParse({ name, email, phone, message });
+
+  if (!parseResult.success) {
+    return Response.json(
+      { error: z.treeifyError(parseResult.error) },
+      { status: 400 },
+    );
+  }
+
+  if (!parseResult.data.name || !parseResult.data.email) {
     return new Response(
       JSON.stringify({ message: "Name and email are required" }),
       { status: 400 },
@@ -11,8 +27,8 @@ export async function POST(req: Request) {
   }
 
   const processedMessage =
-    typeof message === "string" && message.trim().length > 0
-      ? message
+    typeof parseResult.data.message === "string" && parseResult.data.message.trim().length > 0
+      ? parseResult.data.message
       : "Olá, gostaria de entrar em contato.";
 
   try {
@@ -20,17 +36,17 @@ export async function POST(req: Request) {
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD, 
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
 
     await transporter.sendMail({
       from: process.env.EMAIL_USER,
       to: process.env.EMAIL_USER, // Maybe change to another email
-      subject: `[SITE] Mensagem do site de ${name}`,
-      text: `Nome: ${name}
-      Email: ${email}
-      Telefone: ${phone}
+      subject: `[SITE] Mensagem do site de ${parseResult.data.name}`,
+      text: `Nome: ${parseResult.data.name}
+      Email: ${parseResult.data.email}
+      Telefone: ${parseResult.data.phone}
       Mensagem: ${processedMessage}`,
     });
 
